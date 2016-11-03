eacf766d816c941c8060a74467a5de3c3602ca3f
Jonathan Morales Salazar <blonder413@gmail.com>
2016-11-02 20:54:11 -0500

    behaviors

---
+++ views/categoria/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Categoria */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="categoria-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'categoria')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'imagen')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'descripcion')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
